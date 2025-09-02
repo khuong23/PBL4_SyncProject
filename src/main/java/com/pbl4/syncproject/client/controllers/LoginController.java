7ff18eb2117623bb943fe0bd650bacf69963d6ae
khuong23 <baokhuongtran1234@gmail.com>
2025-09-02 22:54:10 +0700

    Add Port and Ip select option

--- src/main/java/com/pbl4/syncproject/client/controllers/LoginController.java
+++ src/main/java/com/pbl4/syncproject/client/controllers/LoginController.java
@@ -4,23 +4,59 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.concurrent.Task;
 import java.io.*;
-import java.net.Socket;
+import java.net.*;
+import java.util.Enumeration;
 
 public class LoginController {
     @FXML private TextField txtUsername;
     @FXML private PasswordField txtPassword;
     @FXML private Label lblStatus;
+    @FXML private ComboBox<String> cmbIp;
+    @FXML private ComboBox<String> cmbPort;
+    @FXML
+
+    public void initialize(){
+        cmbPort.getItems().addAll("5000","21", "22", "80", "443", "8080");
+        cmbPort.getSelectionModel().select(0); // 5000
+        cmbIp.getItems().add("127.0.0.1");
+        loadIpAddress();
+    }
+    private void loadIpAddress(){
+        try {
+            Enumeration<NetworkInterface> nics = NetworkInterface.getNetworkInterfaces();
+            while (nics.hasMoreElements()) {
+                NetworkInterface nic = nics.nextElement();
+                Enumeration<InetAddress> inetAddresses = nic.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    InetAddress inetAddress = inetAddresses.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                        cmbIp.getItems().add(inetAddress.getHostAddress());
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     private void handleLogin() {
         String username = txtUsername.getText();
         String password = txtPassword.getText();
+        String ip = cmbIp.getSelectionModel().getSelectedItem();
 
+        if (ip == null || cmbPort.getSelectionModel().isEmpty()) {
+            lblStatus.setText("⚠️ Vui lòng chọn IP và Port!");
+            lblStatus.setStyle("-fx-text-fill: orange;");
+            return;
+        }
+
+        int port = Integer.parseInt(cmbPort.getSelectionModel().getSelectedItem());
         // Create a Task for the network operation
         Task<String> loginTask = new Task<>() {
             @Override
             protected String call() throws Exception {
-                try (Socket socket = new Socket("localhost", 5000);
+                try (Socket socket = new Socket(ip, port);
                      PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
                      BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
